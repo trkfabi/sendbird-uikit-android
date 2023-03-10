136fbbe5aaa4239eb94805ebc0003d71581d041e
Fabian Martinez <fmartinez@inzori.com>
2023-03-10 15:20:25 +0100

    don't know

--- uikit/src/main/java/com/sendbird/uikit/internal/ui/messages/AdminMessageView.kt
+++ uikit/src/main/java/com/sendbird/uikit/internal/ui/messages/AdminMessageView.kt
@@ -8,6 +8,8 @@ import com.sendbird.android.message.BaseMessage
 import com.sendbird.uikit.R
 import com.sendbird.uikit.databinding.SbViewAdminMessageComponentBinding
 import com.sendbird.uikit.internal.extensions.setAppearance
+import com.sendbird.uikit.utils.DrawableUtils
+import com.sendbird.uikit.utils.ViewUtils
 
 internal class AdminMessageView @JvmOverloads internal constructor(
     context: Context,
@@ -20,7 +22,7 @@ internal class AdminMessageView @JvmOverloads internal constructor(
 
     init {
         val a =
-            context.theme.obtainStyledAttributes(attrs, R.styleable.MessageView_Admin, defStyle, 0)
+            context.theme.obtainStyledAttributes(attrs, R.styleable.MessageView_User, defStyle, 0)
         try {
             binding = SbViewAdminMessageComponentBinding.inflate(
                 LayoutInflater.from(context),
@@ -28,21 +30,38 @@ internal class AdminMessageView @JvmOverloads internal constructor(
                 true
             )
             val textAppearance = a.getResourceId(
-                R.styleable.MessageView_Admin_sb_admin_message_text_appearance,
+                R.styleable.MessageView_User_sb_message_me_text_appearance,
                 R.style.SendbirdCaption2OnLight02
             )
             val backgroundResourceId = a.getResourceId(
-                R.styleable.MessageView_Admin_sb_admin_message_background,
-                android.R.color.transparent
+                R.styleable.MessageView_User_sb_message_me_background,
+                android.R.color.holo_blue_light
             )
+            val messageBackground = a.getResourceId(
+                R.styleable.MessageView_User_sb_message_me_background,
+                R.drawable.sb_shape_chat_bubble
+            )
+            val messageBackgroundTint = android.R.color.holo_blue_light
+
+            binding.contentPanel.background =
+                DrawableUtils.setTintList(context, messageBackground, messageBackgroundTint)
+
             binding.tvMessage.setAppearance(context, textAppearance)
             binding.tvMessage.setBackgroundResource(backgroundResourceId)
+            binding.tvNickname.setAppearance(context, textAppearance)
+            binding.tvNickname.setBackgroundResource(backgroundResourceId)
+            binding.tvSentAt.setAppearance(context, textAppearance)
+            binding.tvSentAt.setBackgroundResource(backgroundResourceId)
+
+
         } finally {
             a.recycle()
         }
     }
 
     fun drawMessage(message: BaseMessage) {
-        binding.tvMessage.text = message.message
+        binding.tvMessage.text = message.message + " TEST"
+        binding.tvSentAt.text = message.createdAt.toString()
+        binding.tvNickname.text = message.sender?.nickname
     }
 }
--- uikit/src/main/java/com/sendbird/uikit/internal/ui/messages/OpenChannelAdminMessageView.kt
+++ uikit/src/main/java/com/sendbird/uikit/internal/ui/messages/OpenChannelAdminMessageView.kt
@@ -28,16 +28,18 @@ internal class OpenChannelAdminMessageView @JvmOverloads internal constructor(
                 this,
                 true
             )
-            val avatar = a.getResourceId(
-                R.styleable.MessageView_File_sb_quoted_message_me_file_icon_tint,
-                R.drawable.icon_members
-            )
-            val messageBackground = a.getResourceId(
-                R.styleable.MessageView_User_sb_message_other_background,
-                R.drawable.sb_shape_chat_bubble
-            )
-            val messageBackgroundTint =
-                a.getColorStateList(R.styleable.MessageView_User_sb_message_other_background_tint)
+//            val avatar = a.getResourceId(
+//                R.styleable.MessageView_File_sb_quoted_message_me_file_icon_tint,
+//                R.drawable.icon_members
+//            )
+//            val messageBackground = a.getResourceId(
+//                R.styleable.MessageView_User_sb_message_other_background,
+//                R.drawable.sb_shape_chat_bubble
+//            )
+//            val messageBackgroundTint =
+//                a.getColorStateList(R.styleable.MessageView_User_sb_message_other_background_tint)
+//            binding.contentPanel.background =
+//                DrawableUtils.setTintList(context, messageBackground, messageBackgroundTint)
             val textAppearance = a.getResourceId(
                 R.styleable.MessageView_Admin_sb_admin_message_text_appearance,
                 R.style.SendbirdCaption2OnLight02
@@ -46,11 +48,10 @@ internal class OpenChannelAdminMessageView @JvmOverloads internal constructor(
                 R.styleable.MessageView_Admin_sb_admin_message_background,
                 R.drawable.sb_shape_admin_message_background_light
             )
-            binding.contentPanel.background =
-                DrawableUtils.setTintList(context, messageBackground, messageBackgroundTint)
+
             binding.tvMessage.setAppearance(context, textAppearance)
-            binding.tvNickname.setAppearance(context, textAppearance)
-            binding.tvSentAt.setAppearance(context, textAppearance)
+//            binding.tvNickname.setAppearance(context, textAppearance)
+//            binding.tvSentAt.setAppearance(context, textAppearance)
 
             //binding.tvMessage.setBackgroundResource(backgroundResourceId)
         } finally {
@@ -60,8 +61,8 @@ internal class OpenChannelAdminMessageView @JvmOverloads internal constructor(
 
     fun drawMessage(message: BaseMessage) {
         binding.tvMessage.text = message.message
-        binding.tvNickname.text = message.sender?.nickname ?: ""
-        binding.tvSentAt.text = message.createdAt.toString()
+//        binding.tvNickname.text = message.sender?.nickname ?: ""
+//        binding.tvSentAt.text = message.createdAt.toString()
 
     }
 }
